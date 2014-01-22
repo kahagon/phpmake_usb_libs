7e0957ae5e3a75f97b9a07d4c44e983de3320003
Keng-ichi Ahagon <k.ahagon@n-3.so>
2014-01-23 01:52:25 +0900

    implement Standard Configuration Descriptor

---
+++ src/PHPMake/USB/Descriptor/Configuration.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHPMake\USB\Descriptor;
+use PHPMake\USB\Descriptor;
+use PHPMake\USB\DescriptorField;
+
+class Configuration extends Descriptor {
+
+    protected function _initDescriptorFields() {
+        return array(
+            new DescriptorField('bLength', 1),
+            new DescriptorField('bDescriptorType', 1),
+            new DescriptorField('wTotalLength', 2),
+            new DescriptorField('bNumInterfaces', 1),
+            new DescriptorField('bConfigurationValue', 1),
+            new DescriptorField('iConfiguration', 1),
+            new DescriptorField('bmAttributes', 1),
+            new DescriptorField('bMaxPower', 1),
+        );
+    }
+
+}
